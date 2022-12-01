31cdac402446a7fbeae6b3d495705879582dda40
BrianEtemesi <etemecci@gmail.com>
2022-12-01 17:43:19 +0000

    pycode correct ambiguous variable name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 import sys
 
 if __name__ == "__main__":
-    l = len(sys.argv)
-    if (l < 2):
-        print("{} arguments.".format(l - 1))
-    elif (l == 2):
+    n = len(sys.argv)
+    if (n < 2):
+        print("{} arguments.".format(n - 1))
+    elif (n == 2):
         print("{} argument:\n{}: {}".format(1, 1, sys.argv[1]))
     else:
-        print("{} arguments:".format(l - 1))
+        print("{} arguments:".format(n - 1))
         i = 1
-        while (i < l):
+        while (i < n):
             print("{}: {}".format(i, sys.argv[i]))
             i += 1
