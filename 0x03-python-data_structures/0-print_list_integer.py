c98de59187d822f931135851153182e1ccfc5ee6
BrianEtemesi <etemecci@gmail.com>
2022-12-03 11:22:22 +0000

    include line to prevent code execution when imported

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for i in my_list:
+            print("{}".format(i))
