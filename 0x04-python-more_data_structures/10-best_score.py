42fa8d9eb131a0c53d3fbf2acc8ecf7ce37b7ce8
BrianEtemesi <etemecci@gmail.com>
2022-12-19 09:16:43 +0000

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    v_list = []
+    for values in a_dictionary.values():
+        v_list.append(values)
+    max_score = v_list[0]
+    for i in v_list:
+        if i > max_score:
+            max_score = i
+    for name, score in a_dictionary.items():
+        if score == max_score:
+            return name
