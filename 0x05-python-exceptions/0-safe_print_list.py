27397591d83a0dbb0955001b6ba08be3663a2051
BrianEtemesi <etemecci@gmail.com>
2023-01-04 11:19:04 +0000

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()
+        return x
+    except:
+        j = 0
+        for i in my_list:
+            j = j + 1
+        print()
+        return j
