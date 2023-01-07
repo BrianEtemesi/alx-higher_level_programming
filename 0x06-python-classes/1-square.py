b67d2b257ffa42f89b43ff0c09c006b32d333467
BrianEtemesi <etemecci@gmail.com>
2023-01-07 20:17:23 +0000

    change size attribute to private

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,6 +8,11 @@ and initialize size as a private attribute
 class Square:
     """defines a class called Square"""
 
-    def __init__(self, size=None):
+    def __init__(self, size):
         """initialize size of square"""
-        self._size = size
+        self.__size = size
+
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
