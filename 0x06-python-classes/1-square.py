7255dc4d14052f6d4a8f3b45059ca3dcd0f7b0d7
BrianEtemesi <etemecci@gmail.com>
2023-01-07 20:18:05 +0000

    remove comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -11,8 +11,3 @@ class Square:
     def __init__(self, size):
         """initialize size of square"""
         self.__size = size
-
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
