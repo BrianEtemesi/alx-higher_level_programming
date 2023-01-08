151dfbf189aadc2db9dc2ca20a8ee5c773c3a17e
BrianEtemesi <etemecci@gmail.com>
2023-01-08 09:03:22 +0000

    remove try/except block

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,10 +10,9 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes size of square."""
-        try:
-            size + 1  # operation raises a TypeError if size is not integer
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = size
-        except TypeError:
+
+        if type(size) != int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
