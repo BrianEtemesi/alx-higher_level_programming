9e48db03a8d59a6e39793102907c9004caffb067
BrianEtemesi <etemecci@gmail.com>
2023-01-10 08:58:48 +0000

    test more edge cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 
-"""Execution of a function that adds 2 integers"""
+"""Execution of a function that adds 2 integers
+foo
+bar
+foo
+"""
 
 
 def add_integer(a, b=98):
-    """adds two integers"""
+    """adds two integers
+    check
+    """
 
     if type(a) != int and type(a) != float:
+        """check if `a` is a float or integer"""
+
         raise TypeError("a must be an integer")
 
     if type(b) != int and type(b) != float:
