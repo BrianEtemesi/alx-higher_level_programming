216e83005d36c520117a59672fa9965ae2f24203
BrianEtemesi <etemecci@gmail.com>
2023-01-15 17:30:37 +0000

    My list source code

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Execution a class representing a list ``MyList`` that inherits
+from the built-in python list.
+"""
+
+
+class MyList(list):
+    """ class representing a list ``MyList`` """
+
+    def print_sorted(self):
+        """sorts list in ascending order and prints"""
+
+        a = list(self)
+        a.sort()
+        print(a)
