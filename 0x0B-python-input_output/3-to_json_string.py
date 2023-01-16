ef34c3a4784bf13c56f41fb3c30b28116c317e45
BrianEtemesi <etemecci@gmail.com>
2023-01-16 13:13:30 +0000

    pycodestyle

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,9 +4,8 @@
 Implementaion of a function that returns the JSON
 representation of an object.
 """
-
-
 import json
+
 
 def to_json_string(my_obj):
     """Returns the JSON representation of an object"""
