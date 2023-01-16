3c41529f133e95e60b76c2c1c1a4515043d9413b
BrianEtemesi <etemecci@gmail.com>
2023-01-16 12:02:16 +0000

    sort indentation error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     """reads a text file and prints it to stdout"""
 
     with open(filename, encoding='utf-8') as a_file:
-       x = a_file.read()
-       print(x, end="")
+        x = a_file.read()
+        print(x, end="")
