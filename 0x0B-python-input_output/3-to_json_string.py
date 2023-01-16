1b896c63ae9290b2392b8c5482fa2edfdbd6dc22
BrianEtemesi <etemecci@gmail.com>
2023-01-16 13:10:18 +0000

    reposition import statement

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import json
 
 """
 Implementaion of a function that returns the JSON
 representation of an object.
 """
 
+
+import json
 
 def to_json_string(my_obj):
     """Returns the JSON representation of an object"""
