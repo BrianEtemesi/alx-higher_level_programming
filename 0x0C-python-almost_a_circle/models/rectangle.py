0fd2ef2b0e7e77d12b972e9f91c0526751eb1d69
BrianEtemesi <etemecci@gmail.com>
2023-01-17 13:06:54 +0000

    add height getter setter methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,27 @@ class Rectangle(Base):
     def width(self, value):
         """updates the width attribute of a rectangle instance"""
 
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """retrieves/returns the height attribute of a rectangle instance"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """updates the height attribute of a rectangle instance"""
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
