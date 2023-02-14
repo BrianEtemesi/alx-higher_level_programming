7ef682976290ccfa6fd0f6a9df5539fb6f066e39
BrianEtemesi <etemecci@gmail.com>
2023-02-14 14:39:48 +0000

    change placeholder

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,5 @@
 -- lists all tables of a database in my mysql server
 -- the database will be passed as an argument of mysql command
 -- query to list tables of a database
-USE %s
+USE $1;
 SHOW TABLES;
