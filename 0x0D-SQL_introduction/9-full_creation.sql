9d2e28341de8536a7e6b55728cf4cff64344ac8e
BrianEtemesi <etemecci@gmail.com>
2023-02-14 19:49:25 +0000

    change Insert syntax

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,10 @@
 -- this script creates a table and saves some entries
--- create table
+-- create table and insert multiple entries
 CREATE TABLE IF NOT EXISTS second_table(
 id INT,
 name VARCHAR(256),
 score INT)
--- insert multiple entries to database
-INSERT INTO second_table (id, name, score)
-VALUES
-		(1, "John", 10),
-		(2, "Alex", 3),
-		(3, "Bob", 14),
-		(4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
