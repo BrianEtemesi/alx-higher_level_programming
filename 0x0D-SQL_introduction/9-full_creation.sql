326979426494695400b6b4ecf2c58f8f59f34d90
BrianEtemesi <etemecci@gmail.com>
2023-02-14 19:34:25 +0000

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- this script creates a table and saves some entries
+-- create table
+CREATE TABLE IF NOT EXISTS second_table(
+id INT,
+name VARCHAR(256),
+score INT)
+-- insert multiple entries to database
+INSERT INTO second_table (id, name, score)
+VALUES
+		(1, "John", 10),
+		(2, "Alex", 3),
+		(3, "Bob", 14),
+		(4, "George", 8);
