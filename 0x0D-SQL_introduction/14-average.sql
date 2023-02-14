4111b7e3b238e88635ff4d63f1e21a11ec2cb931
BrianEtemesi <etemecci@gmail.com>
2023-02-14 21:06:03 +0000

    use AS

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,3 @@
 -- computes the score average of all records in second_table
 -- query to get average of score and save to column `average`
-ALTER TABLE second_table ADD COLUMN average FLOAT;
-UPDATE second_table SET average = (SELECT AVG(score) FROM second_table);
+SELECT AVG(score) AS average FROM second_table;
