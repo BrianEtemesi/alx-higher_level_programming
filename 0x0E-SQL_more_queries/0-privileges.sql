88955c601860b0dc0dee42165f5c89220caf03bb
BrianEtemesi <etemecci@gmail.com>
2023-02-15 14:35:14 +0000

    change to 2 commands

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- script to check the user privilages in a mysql server
 -- query to list all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
