17ac084bcd5d26327814c06fa76928018f407b4d
BrianEtemesi <etemecci@gmail.com>
2023-02-15 16:23:41 +0000

    remove WITH GRANT OPTION

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a mysql server user
 -- query to create user `user_0d_1` and grant all privileges
 CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
