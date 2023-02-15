b20ce04c498e92329f760e7a6b07781fffa79359
BrianEtemesi <etemecci@gmail.com>
2023-02-15 21:20:10 +0000

    remove NATURAL JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- lists all cities stored in database
 -- query to list city by id, name and state name
-SELECT id, name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
-NATURAL JOIN states
+JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id ASC;
