a16d341c22395181b2024fe016a4a62050837221
BrianEtemesi <etemecci@gmail.com>
2023-02-15 14:28:52 +0000

    My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- script to check the user privilages in a mysql server
+-- query to list all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
