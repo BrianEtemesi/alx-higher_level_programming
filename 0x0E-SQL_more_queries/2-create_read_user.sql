c580290eb69d96e466b9e244a7b47e3fecfdf424
BrianEtemesi <etemecci@gmail.com>
2023-02-15 17:05:12 +0000

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- script that creates a database and adds a user
+-- query to create db `hbtn_0d_2` and add user `user_0d_2`
+-- user `user_0d_2` should have only SELECT privileges in the db
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT on *.* TO 'user_0d_2'@'localhost';
