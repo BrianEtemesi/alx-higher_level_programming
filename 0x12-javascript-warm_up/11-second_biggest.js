b49c624e9a85f617654ad1c94456a8c3e1f0935b
BrianEtemesi <etemecci@gmail.com>
2023-03-15 17:52:52 +0000

    typo

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 // searches the 2nd biggest int in list of arguments
 const len = process.argv.length;
 if (len < 4) {
-  console.log(1);
+  console.log(0);
 } else {
   const numList = [];
   for (let i = 2; i < len; i++) {
