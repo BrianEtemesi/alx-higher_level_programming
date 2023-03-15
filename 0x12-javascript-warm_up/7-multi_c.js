33ae8f0e35d5c257d02ebbc499b0938490a08f79
BrianEtemesi <etemecci@gmail.com>
2023-03-15 08:58:57 +0000

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Prints string x number of times. x = argument passed
+const arg = process.argv[2];
+const num = parseInt(arg);
+if (num < 0) {
+  return;
+} else if ((num / num) === 1) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurences');
+}
