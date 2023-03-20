25873734faa6178d2a220abe693a0f86770181b9
BrianEtemesi <etemecci@gmail.com>
2023-03-20 07:51:40 +0000

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// class Rectangle with attributes; width and height
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
