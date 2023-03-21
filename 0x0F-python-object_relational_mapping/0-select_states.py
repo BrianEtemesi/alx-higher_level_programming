1d46a9263ab66064b51a037cea42c75d9543e9db
BrianEtemesi <etemecci@gmail.com>
2023-03-21 19:42:32 +0000

    add table name to query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 
 db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
-query = "SELECT * FROM hbtn_0e_0_usa ORDER BY state.id ASC"
+query = "SELECT * FROM states ORDER BY state.id ASC"
 cur.execute(query)
 results = cur.fetchall()
 for row in results:
