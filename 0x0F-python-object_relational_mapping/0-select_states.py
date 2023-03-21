13a35d7b79874a665ff155896997f1e1f581c77b
BrianEtemesi <etemecci@gmail.com>
2023-03-21 19:37:21 +0000

    change localhost to ipv4

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+db = MySQLdb.connect(host='127.0.0.1:3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
 query = "SELECT * FROM hbtn_0e_0_usa ORDER BY state.id ASC"
 cur.execute(query)
