ed3e0af5b414bdcc8aae315a7989e63430fe38c1
BrianEtemesi <etemecci@gmail.com>
2023-03-22 08:25:24 +0000

    check to match only upper case

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%'"
+    query = "SELECT * FROM states WHERE BINARY name LIKE 'N%'"
     cur.execute(query)
     n_states = cur.fetchall()
     for state in n_states:
