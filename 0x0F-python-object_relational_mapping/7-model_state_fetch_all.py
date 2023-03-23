759946a751f3bafc2bb59a03b6e6158cf2c2a76f
BrianEtemesi <etemecci@gmail.com>
2023-03-23 08:51:02 +0000

    pycodestyle check

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,8 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
 
     # create engine to connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           argv[1], argv[2], argv[3]))
 
     Session = sessionmaker(bind=engine)
     session = Session()
