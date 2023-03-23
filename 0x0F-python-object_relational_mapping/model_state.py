a7179c76426aae0e96b3d04d038209731becb7ad
BrianEtemesi <etemecci@gmail.com>
2023-03-23 07:41:16 +0000

    remove debug comments

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,11 +4,8 @@ Defines a class `State` and an instance `Base = declarative_base():`
 Connects to a MySQL server running on `localhost`
 """
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-# create engine to connect to MySQL server
-# engine = create_engine('mysql://localhost:3306')
 
 # create a declarative base instance
 Base = declarative_base()
@@ -22,11 +19,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-# add the state class to the registry 'importing'
-# Base.registry.register(State)
-
-# create all tables in the MySQL database
-# since we only have `State`, it is the only one created in the mysql db
-# Base.metadata.create_all(bind=engine)
