5ab7886d6eb69a6c6f91dfe1a4bdb35b31246919
BrianEtemesi <etemecci@gmail.com>
2023-03-30 14:04:20 +0000

    curl body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script takes in a URL as and argument, sends a request
+# to the URL and displays the size of the body of the response
+
+curl -s "$1" -w '%{size_download}\n' -o /dev/null
