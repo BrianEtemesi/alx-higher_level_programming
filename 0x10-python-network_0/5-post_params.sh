e2c02198a9de29f8e112744abef838d1565d81a6
BrianEtemesi <etemecci@gmail.com>
2023-03-30 16:45:05 +0000

    fix string typo

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends POST request and key-value pair parameters to a URL
-curl -X POST -d "email=test@gmail.com&subject=I will always be there" "$1"
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
