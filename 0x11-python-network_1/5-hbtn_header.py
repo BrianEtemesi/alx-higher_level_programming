ee434df141e3b42ee85f32b21f43450937a7637b
BrianEtemesi <etemecci@gmail.com>
2023-03-31 21:57:28 +0000

    fix bare except

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,8 +8,5 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
-    except:
-        pass
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
