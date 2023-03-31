60b76828dd781d5293b730294ce05232fbeb4a36
BrianEtemesi <etemecci@gmail.com>
2023-03-31 22:00:09 +0000

    change code

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,7 +6,7 @@ displays the value of a variable in the response header
 import requests
 import sys
 
-
 if __name__ == "__main__":
+
     r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    print(r.headers.get('X-Request-Id'))
