2e8a572f6fc5a47dbbce9bd0aa2d80c24cc213c8
BrianEtemesi <etemecci@gmail.com>
2023-04-29 15:20:03 +0000

    handle error and semistandard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,7 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, (error, response, body) => {
-  console.log('code:', response.statusCode);
+  if (!error) {
+    console.log('code:', response.statusCode);
+  }
 });
