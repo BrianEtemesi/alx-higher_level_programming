73a73a133b3eb8ebabf12d01d3e6754b75462742
BrianEtemesi <etemecci@gmail.com>
2023-05-02 18:56:25 +0000

    sample for async/await protocols

--- 0x14-javascript-web_scraping/111-characters.js
+++ 0x14-javascript-web_scraping/111-characters.js
@@ -7,20 +7,13 @@ const movieId = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-async function printCharacters() {
-  try {
-    const response = await request(url);
-    const film = JSON.parse(response);
-    const actors = film.characters;
-    for await (const actorUrl of actors) {
-      const actorResponse = await request(actorUrl);
-      const actor = JSON.parse(actorResponse);
-      console.log(actor.name);
-    }
+(async () => {
+  try{
+    const response = await request(url, (error, response, body) => {
+    const film = JSON.parse(response.body);  
+    console.log(film);
+	});
   } catch (error) {
-    console.error(error);
+    console.log(error.response.body);
   }
-}
-
-printCharacters();
-
+})();
