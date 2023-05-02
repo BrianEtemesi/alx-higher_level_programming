dea8a729c48fdd5a51dcdcad7027365646f2e331
BrianEtemesi <etemecci@gmail.com>
2023-05-02 09:28:52 +0000

    Star wars Wedge Antilles correction

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,25 +4,19 @@
 const url = process.argv[2];
 const request = require('request');
 
-let occurence = 0;
-let sum = 0;
-for (let i = 1; i < 8; i++) {
-  const movieUrl = url + '/' + i;
-  request(movieUrl, (error, response, body) => {
-    if (!error) {
-      body = JSON.parse(body);
-      const actorList = body.characters;
-      const id = 18;
-      const charApi = 'https://swapi-api.alx-tools.com/api/people/' + id + '/';
-      for (const actor in actorList) {
-        if (actorList[actor] === charApi) {
-          occurence++;
+request(url, (error, response, body) => {
+  if (!error) {
+    body = JSON.parse(body);
+    const movies = body.results;
+    let nFilms = 0;
+    for (const i in movies) {
+      const charList = movies[i].characters;
+      for (const actor in charList) {
+        if (charList[actor].endsWith('/18/')) {
+          nFilms++;
         }
       }
     }
-    sum = sum + i;
-    if (sum === 28) {
-      console.log(occurence);
-    }
-  });
-}
+    console.log(nFilms);
+  }
+});
