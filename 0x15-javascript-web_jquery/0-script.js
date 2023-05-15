295cbb2fc0a90af9763dd9dbe9d50d04a2709ac0
BrianEtemesi <etemecci@gmail.com>
2023-05-15 07:16:40 +0000

    No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  // Get the header element
+  const headerElement = document.querySelector('header');
+
+  // update text color to red
+  headerElement.style.color = '#FF0000';
+}
