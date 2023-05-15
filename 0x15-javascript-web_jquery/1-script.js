0f1f85a61552e634a640fa5d05d912e254d0b982
BrianEtemesi <etemecci@gmail.com>
2023-05-15 08:17:59 +0000

    With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // $(document).ready() ensures that this script runs after
+  // the HTML document has finished loading
+
+  // update text color of header element
+  $('header').css('color', '#FFOOOO');
+});
