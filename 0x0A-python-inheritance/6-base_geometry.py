9bcc978a3338cca98075141fc3f5ae8b5c856e14
BrianEtemesi <etemecci@gmail.com>
2023-06-23 22:54:01 +0000

    improve basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+defines a class with a method area
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
