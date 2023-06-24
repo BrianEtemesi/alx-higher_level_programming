71059e3e3443f7fc07b9b50ea27978fd2a01c39f
BrianEtemesi <etemecci@gmail.com>
2023-06-24 00:02:12 +0000

    add to list and save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that adds all arguments to a Python list, and then
+save them to a file
+"""
+import sys
+from os.path import exists
+
+
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
+
+my_list = []
+if exists("add_item.json"):
+    my_list = load_file("add_item.json")
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+save_file(my_list, "add_item.json")
